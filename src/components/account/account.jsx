017b751c12f36d90470a3f405c64cd99e83293db
mehaksharma23 <mehaksharma2312003@gmail.com>
2024-02-26 20:12:14 +0530

    accounts page

--- src/components/account/account.jsx
+++ src/components/account/account.jsx
@@ -1,27 +1,64 @@
-import { useContext,useState } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../usercontext";
-import {Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { Navbar } from "../nav_bar/nav2";
 import axios from "axios";
-
+import "./account.css";
 export const Account = () => {
-  const { user,setuser } = useContext(UserContext);
+  const { user, setuser } = useContext(UserContext);
   const [isredirect, setisredirect] = useState(null);
-  
+
   const logout = async () => {
     await axios.post("/logout");
-    setisredirect("/")
+    setisredirect("/");
     setuser(null);
   };
 
-  if(isredirect){
-    return <Navigate to={isredirect} />
+  if (isredirect) {
+    return <Navigate to={isredirect} />;
   }
   return (
-    <div>
-      <div>
-        loggin in as {user.name}
-        <button onClick={logout}>logout</button>
-        check this out
+    <div className="main_">
+      <Navbar />
+      <div className="contain">
+        <div className="first_row">
+          <p className="course_heading">
+            Performance Metrics<span id="arrow">>>></span>
+          </p>
+          <div className="box1">
+            <div className="content_box1"></div>
+          </div>
+        </div>
+        <div className="first_row2">
+          <p>user image </p>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+
+          <button onClick={logout}>logout</button>
+        </div>
+      </div>
+
+      <div className="contain">
+        <div className="first_row">
+          <p className="course_heading">
+            Feedback<span id="arrow">>>></span>
+          </p>
+          <div className="box1">
+            <textarea
+              value=""
+              placeholder="Enter your feedback here..."
+              rows={5}
+              cols={100}
+            />
+            <br />
+            <button>Submit Feedback</button>
+          </div>
+        </div>
+        <div className="first_row2">
+          <p className="course_heading">
+            Your Notes<span id="arrow">>>></span>
+          </p>
+        </div>
       </div>
     </div>
   );
