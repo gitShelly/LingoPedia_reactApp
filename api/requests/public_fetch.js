24b5718698770756563a60298085107f79d74a02
gitShelly <avishigupta0719@gmail.com>
2024-05-04 04:03:26 +0530

    chk wuhan

--- api/requests/public_fetch.js
+++ api/requests/public_fetch.js
@@ -7,8 +7,7 @@ const fetchpublicpdf = async (req, res) => {
 
     const pdf_sum=await PublicModel.aggregate([{$group:{_id:"$lang",total_pdfs:{$sum:1}}}]).sort({_id:1})
 
-    const lang_pdf_sum=pdf_sum[langID].total_pdfs
-    console.log(lang_pdf_sum)
+    const lang_pdf_sum = pdf_sum.find(item => item._id === langID)?.total_pdfs || 0;
 
     if (!documents || documents.length === 0) {
       return res.status(404).json({ error: "PDFs not found" });
